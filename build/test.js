1c4de179ad1e6b423346f1ea7273858d5513dfec
Savvin Sergey <savvinsergey.web@gmail.com>
2017-10-27 16:28:58 +0300

    Code was refactored to async/await

--- build/test.js
+++ build/test.js
@@ -1,3 +1,38 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [0, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 /*
  *  Power BI Visualizations
  *
@@ -29,6 +64,7 @@
 // Testrunner
 /// <reference path="./../node_modules/webdriver-client-test-runner/src/webdriver-client-test-runner/typedefs/exports.d.ts" />
 var config = require('../configuration/embedded-reports-urls.config.json');
+var debugMode = process.argv.indexOf("--debug") !== -1;
 var powerbi;
 (function (powerbi) {
     var extensibility;
@@ -39,47 +75,147 @@ var powerbi;
             (function (test) {
                 var imageComparison;
                 (function (imageComparison) {
-                    var existTimeout = 15000, pause = 2500, defaultElement = "div.visual", defaultFrameElement = "svg";
+                    var dafaultExistTimeout = 20000, defaultPause = 3500, defaultElement = "div.visual", defaultFrameElement = "svg", iframeSandboxElement = "iframe.visual-sandbox", pagePaginationElements = ".logoBar .navigation-wrapper > a";
+                    function paginatePages(loop) {
+                        return __awaiter(this, void 0, void 0, function () {
+                            var paginationLinkEl, paginationIconEl, classedOfPaginationEl, err_1;
+                            return __generator(this, function (_a) {
+                                switch (_a.label) {
+                                    case 0:
+                                        _a.trys.push([0, 5, , 6]);
+                                        return [4 /*yield*/, browser.elements(pagePaginationElements)];
+                                    case 1:
+                                        paginationLinkEl = (_a.sent()).value;
+                                        return [4 /*yield*/, browser.elementIdElement(paginationLinkEl[2].ELEMENT, "i")];
+                                    case 2:
+                                        paginationIconEl = _a.sent();
+                                        return [4 /*yield*/, browser.elementIdAttribute(paginationIconEl.value.ELEMENT, "class")];
+                                    case 3:
+                                        classedOfPaginationEl = _a.sent();
+                                        if (classedOfPaginationEl.value.indexOf("inactive") !== -1) {
+                                            return [2 /*return*/];
+                                        }
+                                        return [4 /*yield*/, browser
+                                                .elementIdClick(paginationLinkEl[2].ELEMENT)];
+                                    case 4:
+                                        _a.sent();
+                                        loop();
+                                        return [3 /*break*/, 6];
+                                    case 5:
+                                        err_1 = _a.sent();
+                                        throw new Error(err_1);
+                                    case 6: return [2 /*return*/];
+                                }
+                            });
+                        });
+                    }
+                    function checkIFrame(element, existTimeout) {
+                        return __awaiter(this, void 0, void 0, function () {
+                            var frameElement, frameTimeout, iFrameEl, err_2;
+                            return __generator(this, function (_a) {
+                                switch (_a.label) {
+                                    case 0:
+                                        if (!element ||
+                                            (element && !element.frame)) {
+                                            return [2 /*return*/];
+                                        }
+                                        frameElement = (element && element.frame) || defaultFrameElement;
+                                        frameTimeout = existTimeout || dafaultExistTimeout;
+                                        _a.label = 1;
+                                    case 1:
+                                        _a.trys.push([1, 7, , 8]);
+                                        return [4 /*yield*/, browser.waitForExist(iframeSandboxElement, dafaultExistTimeout - 5000)];
+                                    case 2:
+                                        _a.sent();
+                                        return [4 /*yield*/, browser.element(iframeSandboxElement)];
+                                    case 3:
+                                        iFrameEl = _a.sent();
+                                        return [4 /*yield*/, browser.frame(iFrameEl.value)];
+                                    case 4:
+                                        _a.sent();
+                                        return [4 /*yield*/, browser.waitForExist(frameElement, frameTimeout)];
+                                    case 5:
+                                        _a.sent();
+                                        return [4 /*yield*/, browser.frameParent()];
+                                    case 6:
+                                        _a.sent();
+                                        return [2 /*return*/];
+                                    case 7:
+                                        err_2 = _a.sent();
+                                        throw new Error(err_2);
+                                    case 8: return [2 /*return*/];
+                                }
+                            });
+                        });
+                    }
+                    function takeScreenshot(pause, page, screenshotElement) {
+                        browser
+                            .pause(pause)
+                            .assertAreaScreenshotMatch({
+                            name: "visual_page_" + ++page,
+                            ignore: "antialiasing",
+                            elem: screenshotElement
+                        });
+                    }
                     config.forEach(function (item) {
                         describe(item.name || "Name is not specified", function () {
                             var _loop_1 = function (env) {
                                 it(env, function (done) {
                                     var url = item.environments && item.environments[env];
                                     var isUrl = /^https\:\/\/(app|dxt|msit|powerbi-df)\.(powerbi|analysis-df\.windows)\.(com|net)\/view/.test(url);
+                                    var page = 0;
                                     expect(isUrl).toBe(true);
                                     browser
                                         .timeouts("script", 60000)
                                         .timeouts("implicit", 60000)
                                         .timeouts("page load", 60000);
-                                    browser
-                                        .url(url)
-                                        .waitForExist((item.element && item.element.await) || defaultElement, item.existTimeout || existTimeout)
-                                        .then(function () {
-                                        var framePromise = new Promise(function (resolve) {
-                                            if (!item.element || (item.element && !item.element.frame)) {
-                                                return resolve();
-                                            }
-                                            browser
-                                                .element("iframe.visual-sandbox")
-                                                .then(function (res) { return browser.frame(res.value); })
-                                                .waitForExist((item.element && item.element.frame) || defaultFrameElement, item.existTimeout || existTimeout)
-                                                .frameParent()
-                                                .then(function () {
-                                                resolve();
+                                    var urlPromise = browser.url(url);
+                                    (function loop() {
+                                        var _this = this;
+                                        var element = item.element || null;
+                                        if (element &&
+                                            Object.prototype.toString.call(item.element) === "[object Array]") {
+                                            element = element[page];
+                                        }
+                                        var awaitElement = (element && element.await) || defaultElement;
+                                        var screenshotElement = (element && element.snapshot) || defaultElement;
+                                        var existTimeout = item.existTimeout || dafaultExistTimeout;
+                                        var pause = item.pause || defaultPause;
+                                        (function () { return __awaiter(_this, void 0, void 0, function () {
+                                            var err_3;
+                                            return __generator(this, function (_a) {
+                                                switch (_a.label) {
+                                                    case 0:
+                                                        _a.trys.push([0, 6, , 7]);
+                                                        return [4 /*yield*/, urlPromise];
+                                                    case 1:
+                                                        _a.sent();
+                                                        return [4 /*yield*/, browser.waitForExist(awaitElement, existTimeout)];
+                                                    case 2:
+                                                        _a.sent();
+                                                        return [4 /*yield*/, checkIFrame(element, existTimeout)];
+                                                    case 3:
+                                                        _a.sent();
+                                                        return [4 /*yield*/, takeScreenshot(pause, page, screenshotElement)];
+                                                    case 4:
+                                                        _a.sent();
+                                                        return [4 /*yield*/, paginatePages(loop)];
+                                                    case 5:
+                                                        _a.sent();
+                                                        return [3 /*break*/, 7];
+                                                    case 6:
+                                                        err_3 = _a.sent();
+                                                        if (debugMode) {
+                                                            console.error(err_3.message);
+                                                        }
+                                                        return [3 /*break*/, 7];
+                                                    case 7:
+                                                        browser.call(done);
+                                                        return [2 /*return*/];
+                                                }
                                             });
-                                        });
-                                        framePromise
-                                            .then(function () {
-                                            browser
-                                                .pause(item.pause || pause)
-                                                .assertAreaScreenshotMatch({
-                                                name: "visual",
-                                                ignore: 'antialiasing',
-                                                elem: (item.element && item.element.snapshot) || defaultElement,
-                                            })
-                                                .call(done);
-                                        });
-                                    });
+                                        }); })();
+                                    }());
                                 });
                             };
                             for (var env in item.environments) {
